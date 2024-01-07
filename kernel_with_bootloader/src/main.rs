4d034a906b8e0760a3907857f675bccba992e606
Pius Onobhayedo <piosystems@yahoo.co.uk>
2024-01-07 15:14:51 +0100

    FrameBufferWriter pre-println!

--- kernel_with_bootloader/src/main.rs
+++ kernel_with_bootloader/src/main.rs
@@ -1,6 +1,10 @@
 #![no_std]
 #![no_main]
+
+mod writer;
+
 use bootloader_api::config::Mapping;
+use writer::FrameBufferWriter;
 use x86_64::instructions::hlt;
 
 //Use the entry_point macro to register the entry point function: bootloader_api::entry_point!(kernel_main)
@@ -14,6 +18,17 @@ pub static BOOTLOADER_CONFIG: bootloader_api::BootloaderConfig = {
 bootloader_api::entry_point!(my_entry_point, config = &BOOTLOADER_CONFIG);
 
 fn my_entry_point(boot_info: &'static mut bootloader_api::BootInfo) -> ! {
+
+    let frame_buffer_info = boot_info.framebuffer.as_mut().unwrap().info();
+
+    let buffer = boot_info.framebuffer.as_mut().unwrap().buffer_mut();
+
+    let mut frame_buffer_writer = 
+        FrameBufferWriter::new(buffer, frame_buffer_info);
+
+    use core::fmt::Write;//below requires this
+    writeln!(frame_buffer_writer, "Testing testing {} and {}", 1, 4.0/2.0).unwrap();
+
     loop {
         hlt(); //stop x86_64 from being unnecessarily busy while looping
     }
